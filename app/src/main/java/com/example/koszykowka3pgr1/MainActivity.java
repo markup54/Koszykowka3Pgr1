f5b8eed1a4df2343624948b9401eb3e329ec9567
markup54 <marzenakupka@tlen.pl>
2022-11-10 14:28:24 +0100

    Wiazanie widokow

--- app/src/main/java/com/example/koszykowka3pgr1/MainActivity.java
+++ app/src/main/java/com/example/koszykowka3pgr1/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.koszykowka3pgr1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.koszykowka3pgr1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private Integer punkty=0;
+    private PunktyViewModel punktyViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        punktyViewModel = new ViewModelProvider(this)
+                .get(PunktyViewModel.class);
+
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        binding.textView.setText(punkty.toString());
+                    }
+                }
+        );
     }
 }
